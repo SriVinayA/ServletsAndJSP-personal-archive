9439677b52531aa97aa1aa7b7e76c5b0eb1c3e6a
SriVinayA <srivinay.appari@gmail.com>
2020-08-01 14:54:23 +0530

    HttpSession

--- Servlet_HttpSession/src/com/asv/AddServlet.java
+++ Servlet_HttpSession/src/com/asv/AddServlet.java
@@ -11,11 +11,9 @@ public class AddServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int i = Integer.parseInt(req.getParameter("num1"));
         int j = Integer.parseInt(req.getParameter("num2"));
-        
         int k = i + j;
-        req.setAttribute("k", k);
 
-        RequestDispatcher rd = req.getRequestDispatcher("sq");
-        rd.forward(req, resp);
+        //URL rewriting
+        resp.sendRedirect("sq?k="+k);
     }
 }
--- Servlet_HttpSession/src/com/asv/SqServlet.java
+++ Servlet_HttpSession/src/com/asv/SqServlet.java
@@ -9,7 +9,7 @@ import java.io.PrintWriter;
 
 public class SqServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int k = (int) req.getAttribute("k");
+        int k = Integer.parseInt(req.getParameter("k"));
         k = k*k;
 
         PrintWriter out = resp.getWriter();
