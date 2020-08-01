431dadc2185bbbe09f46c41dd6aba75f67521d65
SriVinayA <srivinay.appari@gmail.com>
2020-08-01 15:11:54 +0530

    HttpSession

--- Servlet_HttpSession/src/com/asv/AddServlet.java
+++ Servlet_HttpSession/src/com/asv/AddServlet.java
@@ -5,15 +5,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AddServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int i = Integer.parseInt(req.getParameter("num1"));
         int j = Integer.parseInt(req.getParameter("num2"));
-        int k = i + j;
 
-        //URL rewriting
-        resp.sendRedirect("sq?k="+k);
+        int k = i + j;
+        HttpSession session = req.getSession();
+        session.setAttribute("k",k);
+
+        resp.sendRedirect("sq");
     }
 }
--- Servlet_HttpSession/src/com/asv/SqServlet.java
+++ Servlet_HttpSession/src/com/asv/SqServlet.java
@@ -4,12 +4,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SqServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int k = Integer.parseInt(req.getParameter("k"));
+
+        HttpSession session = req.getSession();
+        int k = (int) session.getAttribute("k");
+
         k = k*k;
 
         PrintWriter out = resp.getWriter();
