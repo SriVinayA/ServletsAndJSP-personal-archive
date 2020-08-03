bdb9a2b939ed4c502111a83b1cd47011082146e0
SriVinayA <srivinay.appari@gmail.com>
2020-08-03 15:42:00 +0530

    JSP

---
+++ AdditionUsingJSP/src/com/asv/AddServlet.java
@@ -0,0 +1,25 @@
+package com.asv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body bgcolor='cyan'>");
+		out.println("Output = " + k);
+		out.println("</body></html>");
+	}
+}
